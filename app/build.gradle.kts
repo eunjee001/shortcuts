e8cf3811a37aa2f523f4735838cb74e48717b469
eunkyoung99 <eunjee001@gmail.com>
2024-02-01 15:15:11 +0900

    Static Shortcut, Dynamic shortcut, PinnedShortcut 코드 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.core:core-google-shortcuts:1.1.0")
+    implementation ("androidx.work:work-runtime-ktx:2.9.0")
 }
--- app/src/main/java/com/kkyoungs/shortcuts/MainActivity.kt
+++ app/src/main/java/com/kkyoungs/shortcuts/MainActivity.kt
@@ -1,25 +1,50 @@
 package com.kkyoungs.shortcuts
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.content.pm.ShortcutInfoCompat
-import androidx.core.content.pm.ShortcutManagerCompat
-import androidx.core.graphics.drawable.IconCompat
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    private var shortcutManager : ShortcutManager ?=null
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var shortcut = ShortcutInfoCompat.Builder(applicationContext, "id")
-            .setShortLabel("Website")
+        shortcutManager = getSystemService(ShortcutManager::class.java)
+
+        val shortcut = ShortcutInfo.Builder(this, "id")
+            .setShortLabel("naver")
             .setLongLabel("Open the website")
-            .setIcon(IconCompat.createWithResource(applicationContext, R.drawable.ic_launcher_background))
+            .setIcon(Icon.createWithResource(this, R.drawable.ic_launcher_background))
             .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com")))
             .build()
 
-        ShortcutManagerCompat.pushDynamicShortcut(applicationContext, shortcut)
+        shortcutManager?.dynamicShortcuts = listOf(shortcut)
+        addPinnedNaverShortCut()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun addPinnedNaverShortCut(){
+        shortcutManager?.let {shortcutManager->
+            if (shortcutManager.isRequestPinShortcutSupported){
+                val shortcut = ShortcutInfo.Builder(this, "id")
+                    .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com")))
+                    .setShortLabel("google")
+                    .setIcon(Icon.createWithResource(this, R.drawable.ic_launcher_background)).build()
+                val pinnedShortcutCallbackIntent = shortcutManager.createShortcutResultIntent(shortcut)
+                val successCallback = PendingIntent.getBroadcast(this, 0, pinnedShortcutCallbackIntent,
+                    PendingIntent.FLAG_IMMUTABLE)
+                shortcutManager.requestPinShortcut(shortcut, successCallback.intentSender)
+            }
+
+        }
     }
 }
